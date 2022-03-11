3337808c2e4128137d37e53d9fd8566be06e8112
Bright ugwu <brittugu8@gmail.com>
2022-03-12 10:18:12 +1200

    Gaming-App

--- app/src/androidTest/java/com/example/megagames/GameHomeTest.kt
+++ app/src/androidTest/java/com/example/megagames/GameHomeTest.kt
@@ -1,22 +1,51 @@
 package com.example.megagames
 
-import org.junit.Assert.*
-import org.junit.Before
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.filters.MediumTest
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.mock
+import org.mockito.junit.MockitoJUnit
 
+
+@HiltAndroidTest
+@MediumTest
 class GameHomeTest{
 
-    lateinit var gameHome: GameHome
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
 
-    @Before
-    fun setup(){
-        gameHome = GameHome()
-    }
+
+    @Mock
+    var navcontroller = mock(NavController::class.java)
+
+    @get:Rule
+    var mockRule = MockitoJUnit.rule()
 
 
     @Test
     fun launch_fragment_perform_click(){
-        launchFragmentInHiltContainer<GameHome> {  }
+        launchFragmentInHiltContainer<GameHome> {
+            Navigation.setViewNavController(requireView(), navcontroller)
+
+
+        }
+        Thread.sleep(5000)
+        onView(withId(R.id.rv_list)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<GamesAdapter.GamesViewHolder>(
+            0, click()
+        ))
+
 
     }
+
+
 }
--- app/src/debug/java/com/megagames/TestHiltActivity.kt
+++ app/src/debug/java/com/megagames/TestHiltActivity.kt
@@ -1,5 +1,7 @@
 package com.megagames
 
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class TestHiltActivity : AppCompatActivity()
